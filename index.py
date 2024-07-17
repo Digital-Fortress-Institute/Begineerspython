5fd21f4dfaa83e778234de7db1bd0d49b3810b87
Digital-Fortress-Institute <platform@digitalfortressltd.com>
2024-07-17 09:59:41 +0100

    our class

---
+++ index.py
@@ -0,0 +1,558 @@
+# print('Hello World!')
+age = 25
+# first_name= "kifirotoyyu"
+# print(age)
+# name = input("Please Enter your name:")
+# print(name)
+# string
+# address1 = "47 Akowonjo \"road\" Shasha busstoop"
+# address2 = '47 Akowonjo road Shasha \'busstoop\''
+# mynum = {20, 50, 30, 20, 100, 100, 20, True, 1, False, 0, "tunde", "akwomjo"}
+# print(mynum)
+
+# print("Welcome")
+
+# first_name = "John"
+# last_name = "Doe"
+# print(first_name, last_name)
+# print(f" My name is {first_name} {last_name}")
+
+
+
+
+
+# first_name=input('Enter your first name :')
+# last_name =input('Enter your last name : ')
+# phone_number = input('Enter your phone number :')
+# age= input('Enter your age :')
+
+# print(f' My name is {first_name} {last_name} you can contact me on {phone_number} and I am {age} years old')
+
+# num1 = int(input('Enter your first number :'))
+# num2 = int(input('Enter your second number :'))
+# num3 = int(input('Enter your third number :'))
+# print(f'{num1} + {num2} + { num3} = {num1 + num2 + num3}')
+
+# num1 = float('123445')
+# print(type(num1))
+
+# print('Hello world')
+
+# first_name = input("Enter your first name: ")
+# salary = int(input('Enter your salary : '))
+
+# print(f'Hello Mr {first_name} your current salary is {salary}')
+
+# # control flow
+# tax_below = (salary/100) * 5
+# tax_above = (salary/100) * 8
+
+# if salary <= 9999:
+#     print(f'my salary is less than 10000 so my tax charge is {tax_below}')
+# elif salary >= 10000:
+#     print(f'my salary is above than 10000 so my taxx charge is {tax_above}')
+# elif salary == -salary :
+#     print('Invalid salary input')
+
+    
+
+
+
+
+
+# print(f'my salary is less than 10000 so my tax charge is {tax_below}')
+# print(f'my salary is above than 10000 so my taxx charge is {tax_above}')
+
+
+
+
+# strings These are alpha numeric data types
+address = "107, herberty macauly road, off \"university\" road, akoka yuaba lagos state"
+# print(address.reverse())
+# print(address)
+# print(address[2:20:3])
+# print(len(address))
+# # print(address[::-1])
+
+# print(address[73])
+# new_address= address.split()
+# # print(new_address)
+# new_address.reverse()
+# # print(new_address)
+# new_address[3]="Akowonjo"
+# print(new_address)
+# new_address.pop(3)
+# new_address.append('blue')
+# new_address.insert(3, 'pink')
+# print(new_address)
+
+
+# print(new_address)
+# print(address.startswith('1'))
+# print(address.endswith('e'))
+# print(len(address))
+# print(address[0:4])
+# print(address.replace('lagos', 'Ogun'))
+
+# print(address.upper())
+# # print(address)
+
+# List data types
+# x = []
+# fruit_list= ['applee', 'tomatos', 'mango', 'grape']
+# print(type(fruit_list))
+# fruit_list[0]='Orange'
+# fruit_list.append('pawpaw')
+# x.append(fruit_list)
+# fruit_list.insert(2, 'water Melon')
+# fruit_list.pop()
+# fruit_list.remove('apple')
+# fruit_list.sort()
+# fruit_list.clear()
+# del fruit_list
+# x = fruit_list.count('apple')
+
+# if ('apple' in fruit_list):
+#     print('welcome')
+# else:
+#     print('Not welcom')
+
+# print(x)
+
+# assignment solution
+
+# mylist = list()
+# print(type(mylist))
+
+
+# num1 = int(input('Enter your first number'))
+# num2 = int(input('Enter your second number'))
+# print(num1 + num2)
+
+# first_name = input('Enter your first name: ')
+# last_name = input('Enter your last name: ')
+# age= input('Enter your age')
+
+# print(f' Hello MR/MRS/MISS {first_name} {last_name} your age is {age}')
+
+# my_list = ['grape', 'apple', 'mango', 'coconut', 'pineapple']
+# print(my_list[2])
+
+# fruit = ['grape', 'apple', 'mango', 'coconut', 'pineapple']
+# # fruit.append('apple')
+# # print(fruit)
+# new_number= []
+# number = [20, 40, 50, 32, 49, 25]
+# number.pop(2)
+# number.remove(50)
+# number.append(100)
+# new_number.extend(number)
+# number.insert(0, 'banana')
+# print(number)
+
+
+
+fruit = ['grape', 'apple', 'mango', 'banana', 'coconut', 'pineapple']
+# if 'banana' in fruit:
+#     print('yes banana is in the list')
+# else:
+#     print('No banana in the list')
+# # x = str(fruit)
+# print(x[::-1])
+# fruit.reverse()
+# print(fruit)
+# print(x)
+# fruit.sort()
+# print(fruit)
+
+# number = [20, 40, 50, 32, 49, 25]
+# for david in number:
+#     print(david ** 2)
+
+# my_strings = str()
+# print(len(my_strings))
+# print(type(my_strings))
+string1 = 'Pohn is a python eveJloper'
+y = string1.split()
+# print(y)
+# y.reverse()
+# print(y)
+# for x in y:
+#     if x[3] == 'J':
+#         print(x)
+#     else:
+#         print('False')
+    
+# print(string1.split())
+# print(string1.replace('python', 'Javascripts'))
+# print(string1.find('python'))
+# print(string1.reverse())
+# x = string1 
+# print(x[::-1])
+
+
+# print(string1.lower())
+# print(string1.endswith('n'))
+# print(string1.startswith('Z'))
+
+# string2 = 'Doe'
+
+# print(f'{string1} {string2}')
+
+
+my_var = [10, 30, ['digital', ['fortress', 'software devgelopment',['data analysis', ['mango'], 'banana'], 'water melon'], 'frontend'], 50, 70, 90]
+print(my_var[2][1][2][1][0])
+# print(my_var[5])
+# print(my_var[2][0])
+# print(my_var[2][1][3])
+# print(my_var[2][2])
+
+# print out data analysis
+# print out the number 90
+# print out the word digital
+# print out water melon
+# print out frontend
+
+
+
+# my_data = [
+#     {'first_name':'John',
+#      'last_name': 'Doe',
+#      'country': 'USA',
+#      'LGA': 'Washington Dc'
+#      },
+#       {'first_name':'David',
+#      'last_name': 'Desmond',
+#      'country': 'Kenyan',
+#      'LGA': 'Naira Obi'
+#      },
+#       {'first_name':'Tosin',
+#      'last_name': 'Adeleke',
+#      'country': ['Nigeria', "USA", "Germany"],
+#      'LGA': 'Lagos State'
+#      },
+#       {'first_name':'Emeka',
+#      'last_name': 'Prosper',
+#      'country': 'Nigeria',
+#      'LGA': 'Abia'
+#      }
+# ]
+# print(my_data[3]['first_name'])
+
+
+# for wisdom in my_data:
+#     print(wisdom['first_name'])
+# my_data[0]['first_name']='tunde'
+# print(my_data)
+# my_data[2]["country"][2]="Ghana" 
+
+
+# num= [20, 50, 10, 40, 55, 5, 8]
+
+# x = [y for y in num if y > 10]
+# print(x)
+# nnum = 10
+# boolean
+# name = False
+# num1 = 30
+# num2 = 40
+# num3 = 10
+# using comparison operators for boolean statement
+# print(num1 > num2)
+# print(num1 < num2)
+# print(num1 <= num2)
+# print(num1 >= num2)
+# print(num1 == num2)
+# print(num1 !=  num2)
+
+# print(num1 >= num2 - num3)
+
+# if num1 > num2:
+#     print('Yes')
+# else:
+#     print("No")
+
+# print(type(name))
+# dictionary
+
+# employee =  {'first_name':'Emeka',
+#      'last_name': 'Prosper',
+#      'country': 'Nigeria',
+#      'LGA': 'Abia'
+#      }
+# print(employee.values())
+# print(employee.items())
+# employee['country']='USA'
+# employee['occupation'] = " Web Developer"
+# employee['state'] = "Lagos"
+# employee.update({'first_name': 'Tunde'})
+# employee.update({'salary': 100000})
+# employee.pop('country')
+# employee.popitem()
+# print(employee)
+# x = employee.get("first_name")
+# print(x)
+
+# arithmetic operator2
+
+
+# num1 = 60
+# num2 = 40
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 ** 2)
+# print(num1 % num2)
+
+# conditional statement
+# if condition:
+#     print result
+# elif condition:
+#     print result
+
+# elif condition:
+#     print result
+
+# else:
+#     print result
+
+# logical operator
+# or and
+
+# age = 18
+# country = 'Ghana'
+
+# if age <= 17 or country == 'Nigeria':
+#     print('You can Vote')
+# else:
+#     print("You cannot vote")
+
+
+
+# score = 50
+
+# if score > 50:
+#     print('score is greater')
+# elif score == 50:
+#     print('Yes it is 50')
+# else:
+#     print('Less than 50')
+
+
+
+# score = input("Enter your score")
+
+# if score == 'a' or score =='A':
+#     print('Excellent')
+# elif score == 'B' or score =='b':
+#     print('very good')
+# elif score == 'c' or score =='C':
+#     print('Fair')
+# else:
+#     print('Fail')
+
+# if 'a' in score or :
+#     print("Excellent")
+# elif 'b' in score:
+#     print('very good')
+# elif 'c' in score:
+#     print('fair result')
+# elif 'd'in score:
+#     print('fail')
+
+
+# empty_list = []
+# num = list()
+# print(type(num))
+
+# new_color= []
+# color = ['red', 'blue', 'green', 'purple']
+
+# num = [2, 10, 5, 7]
+# x = [wisdom for wisdom in num lamd wisdom ** 2]
+# print(x)
+# for wisdom in num:
+#     print(wisdom ** 2)
+
+# color.reverse()
+# color.sort()
+# print(color)
+# if 'blue' in color:
+#     print('yes, blue is on the list')
+
+# color.append('apple')
+# color.pop(2)
+# color.remove('green')
+# print(color)
+# new_color.extend(color)
+# print(new_color)
+# print(color[2])
+# empty_string=''
+# my_name = str()
+# print(type(my_name))
+# my_place = "26, Akowonjo Street"
+# full_name = 'Digital Forteress'
+# num = [10, 40, 50]
+# x = [y for y in num if y > 10]
+# print(x)
+# print(sum(num))
+# print(min(num))
+# print(max(num))
+# print(full_name.isdigit())
+# for digital in full_name:
+#     print(digital)
+# # print(full_name[::-1])
+# x = full_name.split(' ')
+# x.reverse()
+# print(x)
+# print(full_name.replace('Digital', 'Computer'))
+# print(full_name.index('F'))
+# print(full_name.find('Digital'))
+# print(full_name.upper())
+# print(full_name[4:7])
+# print(len(full_name))
+# print('the current location is'+ ' ' +my_place+ ' '+ ' ' +'where you will find'+ ' ' + full_name)
+# print('the current location is',  my_place, 'where you will find', full_name)
+# print(f' the current location is {my_place} where you will find {full_name}')
+
+# num = int(input('Enter your number  \n'))
+# if num % 3 == 0:
+#     print('the number you entered is divisible by 3 this is FIZZ')
+# elif num % 2 == 0:
+#     print('the number you entered is divisible by 2 this is Buzz')
+# else:
+#     print('invalid number')
+
+# word = ["Training", "Mango", "Tech", "Rock", "technicial", "software" "Analysis", "togo"
+# "Tonga" ]
+# print(type(word))
+# x = str(word)
+# print(type(x))
+
+# for i in word:
+#     # print(i[0])
+#     if i.startswith('t') or i.startswith('T'):
+#     # if i[0] == 't' or i[0] == 'T':
+#         print(i)
+
+# while True:
+#     age =input('Enter your age: ')
+#     if age < '18':
+#         print('You cannot vote')
+#         break
+#     else:
+#         print('you can vote')
+# while True:      
+#     number = int(input('enter your number'))
+
+#     if number > 0:
+#         print('positive')
+#     elif number < 0:
+#         print('nagetive')
+#     else:
+#         print('Zero')
+
+# while loop
+
+# x = 0 #initial
+# num2 = int(input('enter a number'))
+# # number = int(input('Enter a number: '))
+# while x <= 11: # condition
+#     # print(x) #result
+#     x += 1 #increment
+#     wisdom = f'{num2} x {x} = {num2 * x}'
+#     print(wisdom)
+
+
+# import random
+# while True:
+#     num= int(input('Enter your guess number'))
+#     guess = random.randint(0, 4)
+#     print(guess)
+#     if num > guess:
+#         print('Your guess is greater')
+#     elif num < guess:
+#         print('guess is lesser')
+#     else:
+#         print('Congratulation your guess is correct')
+#         exit()
+
+# first_num = int(input('Enter your first number'))
+# second_num = int(input('Enter your second number'))
+# third_num = int(input('Enter your second number'))
+
+# if first_num > second_num and first_num > third_num:
+#     print(f'the first number is the greater which is {first_num}')
+# elif second_num > first_num and second_num > third_num:
+#     print(f'the second number is the greater which is {second_num}')
+# elif third_num > first_num and third_num > second_num:
+#     print(f'third number is the greater {third_num}')
+# else:
+#     print('there are two equal number')
+
+
+
+
+# num=int(input('Enter a number  :  '))
+# for i in range(0, 13):
+#     # print(i)
+#     product = f'{num} x {i} = {num * i}'
+#     print(product)
+
+# while True:
+#     score = int(input('enter tour score: '))
+#     if score >=90 and score <= 100:
+#         print('Grade A')
+#     elif score >= 70 and score <= 89:
+#         print('Grade B')
+#     elif  score >= 50 and score <= 69:
+#         print('Grade c')
+#     elif  score >= 40 and score <= 49:
+#         print('Fail')
+#     else:
+#         print('unknown result')
+#         exit()
+
+
+# salary = int(input('Enter your salary'))
+# tax1 = (salary * 10)/ 100
+# tax2 = (salary * 20)/ 100
+# tax3 = (salary * 30)/ 100
+
+# if salary <= 5000:
+#     print(f'the total salary is {salary} and the tax charge is {tax1}')
+# elif salary >=1000:
+#     print(f'the total salary is {salary} and the tax charge is {tax2}')
+# else:
+#     print(f'the total salary is {salary} and the tax charge is {tax3}')
+
+
+# set data types this is used to store unique data
+
+# number = {10, 20, 40, 50, 80, 30, 10, 50, False, 0, 1, True}
+# print(number)
+
+# tuple data type this is used store data that can not be changed
+# my_tuple = ('apple', 'banana', 'orange', 'mango')
+
+# print(my_tuple)
+# print(my_tuple)
+# x = list(my_tuple)
+# x[0]="Pink"
+# print(x)
+# y=tuple(x)
+# print(y)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ intro.py
@@ -0,0 +1,51 @@
+# print('Hello World')
+
+# x, y, z = 10, 40, 50
+# print(z)
+
+# first_name = 'Tunde'
+# last_name = 'John'
+
+# concatination the process of joining two variable together
+# print(first_name, last_name)
+# print(first_name+ ' '+ last_name)
+# print(f'{first_name} {last_name}')
+
+# creation of programme
+# first_name = input("Please enter your first name : ")
+# last_name = input('Please enter your last name : ')
+# print(f'Good Morning Mr {first_name} {last_name}')
+
+
+# first_number = int(input('Enter the first number'))
+# second_number = int(input("Enter your second number : "))
+
+# print(f'{first_number} + {second_number} = {first_number + second_number}')
+
+# num1 = str(10)
+# print(type(num1))
+
+# data types
+# string
+# list
+# dictionary
+# number
+# tuple
+# set
+#boolean
+
+address = '47, Herbert Macauly Way, Off \'University\' Road, Akoka Yaba Lagos State'
+# print(address[0:10:2])
+# print(address.upper())
+# print(address.lower())
+# print(address.replace('Lagos', 'Ogun'))
+# print(address.startswith('4'))
+# print(address.endswith('e'))
+
+# print(address.isalnum())
+# print(address.index('4'))
+# print(address.find('H'))
+# tunde = address.split(' ')
+# tunde.reverse()
+# print(tunde)
+
