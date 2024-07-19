4a847b12cc2c96b539ad9ae0e88fcc37032a3da2
greenash22 <prerz35arm@gmail.com>
2024-07-19 13:00:37 +0300

    david

---
+++ david.py
@@ -0,0 +1,11 @@
+# print ('hello world boss man')
+# print('life is gud live long and kill more')
+num=int(input('enter a ur favourite num'))
+x=num-2
+y=num*3
+z=x+y
+d=z/10
+print(x)
+print(y)
+print(z)
+print(d)
