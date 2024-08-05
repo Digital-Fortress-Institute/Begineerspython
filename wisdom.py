ee5a87785d247d5cf1909b4f828591845b490bb6
pleindml <wizzyplein60@gmail.com>
2024-08-05 10:33:32 +0100

    wisdom assignment

---
+++ wisdom.py
@@ -0,0 +1,588 @@
+# print('Hello World!')
+# age = 25
+# first_name= "kifirotoyyu"
+# print(age)
+# name = input("Please Enter your name:")
+# print(name)
+# string
+# address1 = "47 Akowonjo \"road\" Shasha busstoop"
+# address2 = '47 Akowonjo road Shasha \'busstoop\''
+# mynum = {20, 50, 30, 20, 100, 100, 20, True, 1, False, 0, "tunde", "akwomjo"}
+# print(mynum)
+# print("Welcome")
+# first_name = "John"
+# last_name = "Doe"
+# print(first_name, last_name)
+# print(f" My name is {first_name} {last_name}")
+# first_name=input('Enter your first name :')
+# last_name =input('Enter your last name : ')
+# phone_number = input('Enter your phone number :')
+# age= input('Enter your age :')
+# print(f' My name is {first_name} {last_name} you can contact me on {phone_number} and I am {age} years old')
+# num1 = int(input('Enter your first number :'))
+# num2 = int(input('Enter your second number :'))
+# num3 = int(input('Enter your third number :'))
+# print(f'{num1} + {num2} + { num3} = {num1 + num2 + num3}')
+# num1 = float('123445')
+# print(type(num1))
+# print('Hello world')
+# first_name = input("Enter your first name: ")
+# salary = int(input('Enter your salary : '))
+# print(f'Hello Mr {first_name} your current salary is {salary}')
+# # control flow
+# tax_below = (salary/100) * 5
+# tax_above = (salary/100) * 8
+# if salary <= 9999:
+#     print(f'my salary is less than 10000 so my tax charge is {tax_below}')
+# elif salary >= 10000:
+#     print(f'my salary is above than 10000 so my taxx charge is {tax_above}')
+# elif salary == -salary :
+#     print('Invalid salary input')
+# print(f'my salary is less than 10000 so my tax charge is {tax_below}')
+# print(f'my salary is above than 10000 so my taxx charge is {tax_above}')
+# strings These are alpha numeric data types
+# address = "107, herberty macauly road, off \"university\" road, akoka yuaba lagos state"
+# print(address.reverse())
+# print(address)
+# print(address[2:20:3])
+# print(len(address))
+# # print(address[::-1])
+# print(address[73])
+# new_address= address.split()
+# # print(new_address)
+# new_address.reverse()
+# # print(new_address)
+# new_address[3]="Akowonjo"
+# print(new_address)
+# new_address.pop(3)
+# new_address.append('blue')
+# new_address.insert(3, 'pink')
+# print(new_address)
+# print(new_address)
+# print(address.startswith('1'))
+# print(address.endswith('e'))
+# print(len(address))
+# print(address[0:4])
+# print(address.replace('lagos', 'Ogun'))
+# print(address.upper())
+# print(address)
+# List data types
+# x = []
+# fruit_list= ['applee', 'tomatos', 'mango', 'grape']
+# print(type(fruit_list))
+# fruit_list[0]='Orange'
+# fruit_list.append('pawpaw')
+# x.append(fruit_list)
+# fruit_list.insert(2, 'water Melon')
+# fruit_list.pop()
+# fruit_list.remove('apple')
+# fruit_list.sort()
+# print(fruit_list)
+# fruit_list.clear()
+# del fruit_list
+# x = fruit_list.count('apple')
+# if  ('apple' in fruit_list):
+#     print('welcome')
+# else:
+#     print('Not welcom')
+# print(x)
+# assignment solution
+# mylist = list()
+# print(type(mylist))
+# num1 = int(input('Enter your first number'))
+# num2 = int(input('Enter your second number'))
+# print(num1 + num2)
+# first_name = input('Enter your first name: ')
+# last_name = input('Enter your last name: ')
+# age= input('Enter your age')
+# print(f' Hello MR/MRS/MISS {first_name} {last_name} your age is {age}')
+# my_list = ['grape', 'apple', 'mango', 'coconut', 'pineapple']
+# print(my_list[2])
+# fruit = ['grape', 'apple', 'mango', 'coconut', 'pineapple']
+# fruit.append('apple')
+# print(fruit)
+# new_number= []
+# number = [20, 40, 50, 32, 49, 25]
+# number.pop(2)
+# number.remove(50)
+# number.append(100)
+# new_number.extend(number)
+# fruit = ['grape', 'apple', 'mango', 'banana23', 'coconut', 'pineapple']
+# if 'banana' in fruit:
+#     print('yes banana is in the list')
+# else:
+#     print('No banana in the list')
+# x = str(fruit)
+# print(x[::-1])
+# # fruit.reverse()
+# print(x)
+# fruit.sort()
+# print(fruit)
+# number = [20, 40, 50, 32, 49, 25]
+# for david in number:
+#     print(david ** 2)
+# my_strings = str()
+# print(len(my_strings))
+# print(type(my_strings))
+# string1 = 'John is a python developer'
+# y = string1.split()
+# y.reverse()
+# print(y)
+#  for i in y:
+#     if i[0] == 'J':
+#         print(i)
+#     else:
+#         print('False')
+# print(string1.split())
+# print(string1.replace('python', 'Javascripts'))
+# print(string1.find('python'))
+# print(string1.reverse())
+# x = string1
+# print(x[::-1])
+# print(string1.lower())
+# print(string1.endswith('n'))
+# print(string1.startswith('Z'))
+# string2 = 'Doe'
+# print(f'{string1} {string2}')
+# my_var = [10, 30, ['digital', ['fortress', 'software devgelopment',['data analysis', ['mango'], 'banana'], 'water melon'], 'frontend'], 50, 70, 90]
+# print(my_var[2][1][2][0])
+# print(my_var[5])
+# print(my_var[2][0])
+# print(my_var[2][1][3])
+# print(my_var[2][2])
+# print out data analysis
+# print out the number 90
+# print out the word digital
+# print out water melon
+# print out frontend
+# my_data = [
+#     {'first_name':'John',
+#      'last_name': 'Doe',
+#      'country': 'USA',
+#      'LGA': 'Washington Dc'
+#      },
+#       {'first_name':'David',
+#      'last_name': 'Desmond',
+#      'country': 'Kenyan',
+#      'LGA': 'Naira Obi'
+#      },
+#       {'first_name':'Tosin',
+#      'last_name': 'Adeleke',
+#      'country': ['Nigeria', "USA", "Germany"],
+#      'LGA': 'Lagos State'
+#      },
+#       {'first_name':'Emeka',
+#      'last_name': 'Prosper',
+#      'country': 'Nigeria',
+#      'LGA': 'Abia'
+#      }
+# ]
+# for wisdom in my_data:
+#     print(wisdom['first_name'])
+# my_data[0]['first_name']='tunde'
+# print(my_data)
+# my_data[2]["country"][2]="Ghana"
+# num= [20, 50, 10, 40, 55, 5, 8]
+# x = [y for y in num if y > 10]
+# print(x)
+# num = 10
+# boolean
+# name = False
+# print(type(name))
+# dictionary
+# employee =  {'first_name':'Emeka',
+#      'last_name': 'Prosper',
+#      'country': 'Nigeria',
+#      'LGA': 'Abia'
+#      }
+# print(employee.values())
+#print(employee.items())
+# print(employee.keys())
+# employee.pop('country')
+# employee['country']='USA'
+# employee['occupation'] = " Web Developer"
+# employee.update({'first_name': 'Tunde'})
+# employee.update({'salary': 100000})
+# employee.popitem()
+# print(employee)
+# x = employee.get('first_name')
+# print(x)
+
+# python operators
+# Python has several types of operators, each serving a different purpose:
+
+# Arithmetic Operators: Used for basic mathematical operations like addition (+), subtraction (-), multiplication (*), division (/), modulus (%), exponentiation (**), and floor division (//).
+
+# Assignment Operators: Used to assign values to variables, such as the basic assignment (=), addition assignment (+=), subtraction assignment (-=), and others like *=, /=, %= etc.
+
+# Comparison Operators: Used to compare two values, including equal to (==), not equal (!=), greater than (>), less than (<), greater than or equal to (>=), and less than or equal to (<=).
+
+# Logical Operators: Used to combine conditional statements, including and, or, and not.
+# age=18
+# country='nigeria'
+
+# if age >= 18 or country == 'nigeria':
+#     print('you can vote')
+# else:
+#     print('you cannot vote')    
+
+# score= input('enter your score:')
+
+# if score == 'a'or score == 'A':
+#     print('excellent')
+# elif score ==  'b' or score == 'B' :
+#     print('very good')
+# elif score == 'c'or score == 'C':
+#     print('fair')
+# else:
+#     print('fail')   
+
+
+
+
+# Identity Operators: Used to compare the memory locations of two objects, with is and is not.
+
+# Membership Operators: Used to test whether a value is present in a sequence, such as in and not in.
+
+# Bitwise Operators: Used to perform bit-by-bit operations on integers, including AND (&), OR (|), XOR (^), NOT (~), left shift (<<), and right shift (>>)
+
+# conditional statement
+# if condition:
+# print result
+#   elif condition:
+# print result
+# else:
+# print result
+
+# score = 50
+
+# if score > 50:
+#     print('score is greater')
+# elif  score== 50:
+#     print('yes it is 50')
+# else:
+#     print('less than 50')
+
+# word = ["Training", "Mango", "Tech", "Rock", "technicial", "software" "Analysis", "togo"
+# "Tonga" ]
+# print(type(word))
+# x = str(word)
+# print(type(x))
+# for i in word:
+#     # print(i[0])
+#     if i.startswith('t') or i.startswith('T'):
+#     # if i[0] == 't' or i[0] == 'T':
+#         print(i)
+
+
+
+
+# # for i in range(0, 10):
+# #     print(i)
+
+# print(person[0]["country"][3][0])
+# print(person[1]["country"][3][1][0])
+# print(person[1]["country"][3][1][1][0])
+
+# num= int(input('what is your number'))
+# if num % 3 == 0:
+#     print('buzz')
+# else:
+#     print('fizz') 
+
+# age=input('enter yout age:')
+# if age < "18":
+#     print('you cannot vote')
+# else:
+#     print('you can vote')
+
+# number=input('enter your number:')
+# if number > '0':
+#         print('positive')
+# elif number < '0':
+#         print ('negative') 
+# else:
+#         print('zero')    
+# num1=input('enter your first number:')
+# num2=input('enter your second number:')
+# num3=input('enter your third number:')
+# if num1 > num2 and num1 > num3:
+#     print(f'{num1} is greater')
+# elif num2>num1 and num2>num3:
+#     print(f'{num2} is greater' )
+# elif num3>num2 and num3>num1:
+#     print(f'{num3} 3rd number is greater ')
+# else:
+#     print('there are two equal numbers')
+# Grade=input('enter your grade:') 
+# if Grade in range(90,100):
+#         print("Grade A")
+# grade = int(input('grade:'))
+# if grade in range(90,100):
+#     print('Grade A')
+# elif grade in range(70,89):
+#     print('Grade B')
+# elif grade in range(50,69):
+#     print('Grade C')
+# elif grade in range(40,49):
+#     print('Fail')
+# else:
+#     print('Unknown Result')
+
+# salary=int(input('how much is your salary:'))
+
+
+# if salary <= 5000:
+#     print('your tax is 10%')
+# elif salary in range(5001,10000):
+#      print('your tax is 20%')
+# else:
+#     print('your tax is 30%')
+
+
+# num1=int(input('enter your first number:'))
+# num2=int(input('enter your second number:'))
+
+# x= num1 + num2
+
+# if x > 100:
+#     print('sum is greater than 100')
+# else:
+#     print('sum is not greater than 100')
+
+# def myfunc():
+#     name =input('Enter your full name:')
+#     print(name[0:5])
+# myfunc()
+
+# def myfunc():
+#     address=input('Enter your address:')
+#     x=address.split()
+#     print(x[:-1])
+# myfunc()    
+
+# HANDLING ERROR
+ # tuple data type this is used store data that can not be changed
+# my_tuple = ('apple', 'banana', 'orange', 'mango')
+
+# print(my_tuple)
+# print(my_tuple)
+# x = list(my_tuple)
+# x[0]="Pink"
+# print(x)
+# y=tuple(x)
+# print(y)
+
+
+# functions
+# def my_class():
+#     print("Hello world")
+# my_class()
+# my_class()
+# my_class()
+# my_class()
+# my_class()
+
+# def tunde():
+#     num1 = int(input("Enter your first number"))
+#     num2 = int(input("Enter your second number"))
+#     print(f'{num1} + {num2} = {num1 + num2}')
+# tunde()
+
+
+# def pytho_class(name):
+#     return f'My name is {name}'
+
+# print(pytho_class("Tunde"))
+# print(pytho_class("Prosper"))
+# print(pytho_class("Pelumi"))
+
+
+
+
+# def my_address():
+#     address = input('Enter your address')
+#     x=address.split(' ')
+#     print(x.pop())
+#     # address.pop()
+
+# my_address()
+
+# def welcome(x):
+#     # try:
+#         return x / 0
+#     except Exception as w:
+#         return w
+    # except ValueError:
+    #     print("String can not be evaluated")
+    # except ZeroDivisionError:
+    #     print("Number cannot be divided by zero")
+    # except TypeError:
+    #     print("Please enter a valid number")
+    
+
+# print(welcome(20))
+# print('Programme successfully executed')
+
+
+# def my_score():
+    
+#         score = 0
+#         try:
+#             score = int(input("Enter your score"))
+#         except Exception as e:
+#              return e
+#         except ValueError:
+#             print("Enter a valid number")
+#         except TypeError:
+#             print("Make it a number")
+#         match score:
+#             case score if 90 <= score <= 100:
+#                 print("Excellent")
+#             case score if 70 <= score <= 89:
+#                 print("Good Result")
+#             case score if 40 <= score <= 69:
+#                 print("Grade C")
+#             case score if 30 <= score <= 39:
+#                 print("Fail")
+#             case score:
+#                 print("Unknown")
+
+# my_score()
+# print('successfull')
+
+# def sum_class(x, y):
+#     return f'{x} + {y} = {x + y}'
+
+
+# print(sum_class(30, 60))
+
+# def my_function():
+#     name=input('Enter your full name')
+#     print(name[0:5])
+# my_function()
+
+# def add_num(x,y):
+#     try:
+#         return x + y
+#     except ValueError:
+#         print('value must be an integer')
+# def sub(x,y):
+#     try:
+#         return x - y
+#     except ValueError:
+#         print('value must be an integer')
+# def mul(x,y):
+#     try:
+#         return x * y
+#     except ValueError:
+#         print('value must be an integer')
+# def div(x,y):
+#     try:
+#         return x / y
+#     except ValueError:  
+#         print('value must be an integer')
+
+# def calculation():
+#     while True:
+#         choice= (input('''
+#             choice 1 +
+#             choice -
+#             choice *
+#             choice /
+#             choice q
+#         '''))
+#         if choice == 'q' or choice == 'Q':
+#             print('thank you')
+#             exit()
+
+#         first_num=int(input("Enter your first number"))
+#         second_num = int(input('Enter your second number'))
+#         if choice == "1":
+#             print(f'{first_num} + {second_num} = {add_num(first_num, second_num)}')
+#         elif choice == "-":
+#             print(f'{first_num} - {second_num} = {sub(first_num, second_num)}')
+#         elif choice == "*":
+#             print(f'{first_num} * {second_num} = {mul(first_num, second_num)}')
+#         else:
+#             print(f'{first_num} / {second_num} = {sub(first_num, second_num)}')
+        
+            
+# calculation()
+   
+# while loop
+# x = 0 #initial
+# number = int(input('Enter a number: '))
+# while x < 10: # condition
+#     print(x) #result
+#     x += 1 #increment
+#     i= f'{number} x {x}'=
+# while True:
+#  name_of_game=input('guess the game:')
+#  guess = 'soccer'
+#  if name_o f_game== guess:
+#     print('congratulations you guessed right')
+#     exit()
+#  else:
+#       print('wrong')
+
+balance=90000
+card_pin=1111
+card_cvv=567  
+
+while True:
+    try:
+
+        pin= int(input('Enter your pin:'))
+        cvv= int(input('Enter your cvv:'))
+        if pin != card_pin and cvv != card_cvv:
+            print('Enter a valid input')
+        elif pin == card_pin and cvv == card_cvv:
+             print('WELCOME TO GT BANK!, WHAT TRANSACTION WOULD YOU LIKE TO PERFORM?') 
+             
+             break
+        
+    except Exception as e:
+        print('Input must be a number')
+        
+        
+    
+
+while True:
+    try:
+            
+                 
+        choice=int(input('''
+        choice 1 Account Balance
+        choice 2 Withdrawal        
+        choice 3 Deposit    
+        select an option(1, 2 or 3):'''))
+            
+        if choice == 1:
+                                    
+                                    print(f'YOUR ACCOUNT BALANCE IS {balance}')
+                
+        elif choice == 2:
+                                    withdrawal_amount=int(input('input withdrawal amount:'))
+                                    if withdrawal_amount > balance:
+                                        print('INSUFFICIENT BALANCE!')
+                                    else :     
+                                        print(f'WITHDRAWAL SUCCESSFUL! YOUR NEW BALANCE IS NOW {balance - withdrawal_amount} ')
+        elif choice==3:
+                                    deposit=int(input('Input deposit amount:'))
+                                    if deposit < 500:
+                                            print('DEPOSIT AMOUNT MUST BE ABOVE 500')
+                                    else:
+                                     print(f'DEPOSIT SUCCESSFUL! YOUR NEW BALANCE IS {balance + deposit }')
+        next_transaction= int(input('''DO YOU WANT TO PERFORM ANOTHER TRANSACTION?
+                                    1 YES
+                                    2 NO              
+        select an option(1,2):'''))
+        if next_transaction== 2:
+                        print('THANK YOU FOR BANKING WITH US!')
+                        quit()
+
+    except Exception as e:
+        print('Input must be a number')
+                                        
+            
